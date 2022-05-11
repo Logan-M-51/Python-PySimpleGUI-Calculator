6c825fde3e5fc8225cdddc411c1146f6d6e6a70e
Logan <105079040+Logan-M-51@users.noreply.github.com>
2022-05-10 21:33:46 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,66 @@
+import PySimpleGUI as sg
+
+def create_window(theme):
+
+
+    sg.theme(theme)
+    sg.set_options(font = 'Franklin', button_element_size = (6, 3))
+    button_size = (6,3)
+
+    layout = [  
+            #[sg.Text('output', font = 'Franklin 26', justification = 'center', expand_x = True)],
+            [sg.Push(), sg.Text('output', font = 'Franklin 26', right_click_menu = theme_menu, key = '-TEXT-')],
+            [sg.Button('Clear', expand_x = True), sg.Button('Enter', expand_x = True)],
+            [sg.Button(7, size = button_size), sg.Button(8, size = button_size), sg.Button(9, size = button_size), sg.Button('*', size = button_size)],
+            [sg.Button(4, size = button_size), sg.Button(5, size = button_size), sg.Button(6, size = button_size), sg.Button('/', size = button_size)],
+            [sg.Button(1, size = button_size), sg.Button(2, size = button_size), sg.Button(3, size = button_size), sg.Button('-', size = button_size)],
+            [sg.Button(0, expand_x = True), sg.Button('.', size = button_size), sg.Button('+', size = button_size)],
+            ]
+
+    return sg.Window('Calculator', layout)
+theme_menu = ['menu', ['LightGrey1', 'Topanga', 'DarkGray8', 'Random']]
+window = create_window('Topanga')
+
+current_num = []
+full_operation = []
+
+while True:
+
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event in theme_menu[1]:
+        window.close()
+        window = create_window(event)
+
+    if event in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.']:
+        current_num.append(event)
+        num_string = ''.join(current_num)
+        window['-TEXT-'].update(num_string)
+
+    if event in ['+', '-', '*', '/']:
+        full_operation.append(''.join(current_num))
+        current_num = []
+        full_operation.append(event)
+        window['-TEXT-'].update(event)
+
+
+    if event == 'Clear':
+        current_num = []
+        full_operation = []
+        window['-TEXT-'].update('')
+
+    if event == 'Enter':
+        full_operation.append(''.join(current_num))
+        print(full_operation)
+        result = eval(' '.join(full_operation))
+        window['-TEXT-'].update(result)
+
+        #continue operations with resulting number
+        full_operation = []
+        current_num = [str(x) for x in str(result)]
+        print(full_operation)
+        print(current_num)
+
+window.close()
